f41c9b2c9210b3e847d9e35fe6e5293817f9ea8e
leo5852 <leo5852>
2021-04-19 07:35:50 +0900

    .

--- doom/HighNoon.cpp
+++ doom/HighNoon.cpp
@@ -1,8 +1,9 @@
 ﻿#include <bangtal.h>
 
-SceneID startScene, instruction;
-ObjectID startButton, title , playButton, ok, deadeye;
-SoundID startBGM;
+SceneID startScene, instruction, hollywood, hanamura, volskaya;
+ObjectID startButton, title , playButton, ok, deadeye, hand, trooper[3], bas[2], ori;
+SoundID startBGM, hollywoodBGM, volskayaBGM, hanamuraBGM;
+TimerID timer1;
 
 ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown) {
 	ObjectID object = createObject(image);
@@ -15,13 +16,45 @@ ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown
 	return object;
 }
 
+SoundID BGM(const char* mp3, bool play) {
+	SoundID sound = createSound(mp3);
+	if (play) {
+		playSound(sound);
+	}
+	return sound;
+}
+
+
+
+void game_stage(SceneID scene, SoundID bgm, const char* mp3, float time) {
+	enterScene(scene);
+	bgm = BGM(mp3, true);
+	
+	hand = createObject("Images/hand.png", scene, 800, 0, true);
+	deadeye = createObject("Images/deadeye.png", scene, 540, 10, false);
+
+	setTimer(timer1, time);
+	showTimer(timer1);
+	startTimer(timer1);
+
+	if (scene == hollywood) {
+
+	}
+	else if (scene == hanamura) {
+
+	}
+	else if (scene == volskaya) {
+
+	}
+}
+
 void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 	if (object == playButton) {
 		enterScene(instruction);
 	}
 	else if (object == ok) {
 		stopSound(startBGM);
-	
+		game_stage(hollywood, hollywoodBGM, "Sound/hollywoodBGM.mp3", 20.0f);
 	}
 }
 
@@ -34,13 +67,17 @@ int main() {
 	
 	startScene = createScene("시작화면", "start.jpg");
 	instruction = createScene("미션설명", "instruction.png");
+	hollywood = createScene("할리우드", "hollywood.jpg");
+	hanamura = createScene("하나무라", "hanamura.jpg");
+	volskaya = createScene("볼스카야", "volskkaya.jpg");
 
-	startBGM = createSound("Sound/startBGM.mp3");
-	playSound(startBGM);
+	startBGM = BGM("Sound/startBGM.mp3", true);
 	
 	title = createObject("Images/title.png", startScene, 30, 550, true);
 	playButton = createObject("Images/play.png", startScene, 50, 470, true);
 	ok = createObject("Images/ok.png", instruction, 1100, 20, true);
+	
+	timer1 = createTimer(55.0f);
 
 	startGame(startScene);
 }
